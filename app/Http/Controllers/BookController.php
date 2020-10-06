ac70d75c9c131dc0d8c7982959ec9f939cccdc62
Faathir Rahman <r3faathir123@gmail.com>
2020-10-06 22:23:38 +0700

    implementasi book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Book;
+use Validator;
+
 
 class BookController extends Controller
 {
@@ -14,7 +16,6 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
         return Book::get();
     }
 
@@ -36,14 +37,20 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        return Book::create([
-            "title"=>$request->title,
-            "description"=>$request->description,
-            "author"=>$request->author,
-            "publisher"=>$request->publisher,
-            "date_of_issue"=>$request->date_of_issue,
-        ]);
+        $isLoggedIn = auth()->user();
+        if ($isLoggedIn) {
+            $data = Book::create([
+                "title" => $request->input('title'),
+                "description" => $request->input('description'),
+                "author" => $request->input('author'),
+                "publisher" => $request->input('publisher'),
+                "date_of_issue" => $request->input('date_of_issue')
+            ]);
+            
+            return response(['message' => 'Create data success', 'data' => $data], 201);
+        } else {
+            return response(['message' => 'Not authenticated', 'data' => null], 401);
+        }
     }
 
     /**
@@ -78,13 +85,20 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        return Book::find($id)->update([
-            "title" => $request->input('title'),
-            "description" => $request->input('description'),
-            "author" => $request->input('author'),
-            "publisher" => $request->input('publisher'),
-            "date_of_issue" => $request->input('date_of_issue')
-        ]);
+        $isLoggedIn = auth()->user();
+        if ($isLoggedIn) {
+            $data = Book::find($id)->update([
+                "title" => $request->input('title'),
+                "description" => $request->input('description'),
+                "author" => $request->input('author'),
+                "publisher" => $request->input('publisher'),
+                "date_of_issue" => $request->input('date_of_issue')
+            ]);
+            return response(['message' => 'Update data success', 'data' => $data], 201);
+
+        } else {
+            return response(['message' => 'Not authenticated', 'data' => null], 401);
+        }
     }
 
     /**
@@ -95,6 +109,13 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        return Book::destroy($id);
+        $isLoggedIn = auth()->user();
+        if ($isLoggedIn) {
+            $data = Book::destroy($id);
+            return response(['message' => 'Delete data success', 'data' => $data], 201);
+            
+        } else {
+            return response(['message' => 'Not authenticated', 'data' => null], 401);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Route::get('books','BookController@index');
 // Route::post('books','BookController@store');
 // Route::get('books/{id}','BookController@show');
